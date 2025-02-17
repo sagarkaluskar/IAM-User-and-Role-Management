f9d40fde7a2350faf14c075684f75fccee336bd6
sagarkaluskar <sagar_kaluskar@yahoo.com>
2025-02-17 16:15:31 +0000

    IAM Python code3

--- IAM-User-and-Role-Management.py
+++ IAM-User-and-Role-Management.py
@@ -1,17 +1,25 @@
 import boto3
-#boto3 is amodule used to interact with AWS. 
+#boto3 is a module used to interact with AWS. 
 from botocore.exceptions import ClientError
 
 #program to check existing IAM users
+
 aws_management_console = boto3.session.Session(profile_name="default")
+
 #Opening management console. It's a variable. Using AWS config default profile was created. 
+
 iam_console = aws_management_console.resource('iam')
+
 #Opening iam console from management console. Resource IAM. 
 
 for each_user in iam_console.users.all():
+
     #accessing all users from IAM console
+
     print (each_user.name)
+
     #printing all the users name
+
 #program to check existing IAM users
 
 
