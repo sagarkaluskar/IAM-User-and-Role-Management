d4f76363fd16f3377fa8fe2e0ac90cb1b4f04406
sagarkaluskar <sagar_kaluskar@yahoo.com>
2025-02-18 19:50:50 +0000

    add new file

--- IAM-User-and-Role-Management.py
+++ IAM-User-and-Role-Management.py
@@ -5,70 +5,38 @@ import json
 
 from botocore.exceptions import ClientError
 
-#1 PROGRAM TO GET LIST OF EXISTING IAM USERS
-
-aws_management_console = boto3.session.Session(profile_name="default")
-
-#Open management console. It's a variable. Using AWS config default profile was created. 
-
-iam_console = aws_management_console.resource('iam')
-
-#Opening iam console from management console. Resource IAM. 
-
-for each_user in iam_console.users.all():
-
-    #accessing all users from IAM console
-
-    print (each_user.name)
-
-    #printing all the users name
-
-#1 PROGRAM TO GET LIST OF EXISTING IAM USERS
-
-#2 IAM USERS LIST WITH RESOURCE OBJECTS
-
-iam_console_resource = aws_management_console.resource ('iam') #resource object
-
-for each_user in iam_console_resource.users.all():
-    print (each_user.name)
-
-#2 IAM USERS LIST WITH RESOURCE OBJECTS
-
-#3 IAM USERS LIST WITH CLIENT OBJECTS
-
-iam_console_client = aws_management_console.client('iam') #client object
-
-for user in iam_console_client.list_users()['Users']:
-    print(user['UserName'])
-
-#3 IAM USERS LIST WITH CLIENT OBJECTS
-
-#3 CREATE AN IAM USER
-
 iam = boto3.client("iam")
 
 #boto3.client() is a method provided by the Boto3 library (the AWS SDK for Python) that creates a client object for a specific AWS service. The client() method is used to interact with AWS services, allowing you to make API requests to the service. By passing "iam" to boto3.client(), you're specifying that you want to interact with IAM.
 
-#response = iam.create_user(UserName="sskaluskar")
+#1 CREATE AN IAM USER
+
+#response = iam.create_user(UserName="test-user")
 
 #iam.create_user(UserName=username) is a method call on the iam object, which is an instance of the Boto3 IAM client. UserName=username is a keyword argument that is passed to the create_user() function. The username variable holds the value of the name you want to assign to the new IAM user. response is a variable that stores the response object returned by the create_user() function.
 
 #print(response)
-   
-#3 CREATE AN IAM USER
 
-#4 Attach a Policy to a User
+#Initializes an IAM client.
+#Calls create_user() to create a new IAM user named test-user.
+   
+#1 CREATE AN IAM USER
+
+#2 Attach a Policy to a User
 
 response = iam.attach_user_policy(
-    UserName='sskaluskar',
+    UserName='test-user',
     PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
 )
 
 #print(response)
 
-#4 Attach a Policy to a User
+#Uses attach_user_policy() to attach an existing AWS-managed policy to a user.
+#The PolicyArn is the Amazon Resource Name (ARN) for the predefined AmazonS3ReadOnlyAccess policy.
 
-#4 Create an IAM Role
+#2 Attach a Policy to a User
+
+#3 Create an IAM Role
 
 trust_policy = {
     "Version": "2012-10-17",
@@ -83,11 +51,60 @@ trust_policy = {
 
 #Define trust policy (Allows EC2 to assume the role)
 
-response = iam.create_role(
+#response = iam.create_role(
+#    RoleName='EC2_S3_Access_Role',
+#    AssumeRolePolicyDocument=json.dumps(trust_policy)
+#)
+
+#print(response)
+
+#Creates an IAM role with create_role()
+#The AssumeRolePolicyDocument defines the trust relationship allowing EC2 instances to assume the role.
+
+#3 Create an IAM Role
+
+#4 Attach a Policy to a Role
+
+response = iam.attach_role_policy(
     RoleName='EC2_S3_Access_Role',
-    AssumeRolePolicyDocument=json.dumps(trust_policy)
+    PolicyArn='arn:aws:iam::aws:policy/AmazonS3FullAccess'
 )
 
 print(response)
 
-#4 Create an IAM Role
+#Uses attach_role_policy() to grant full S3 access to the IAM role.
+
+#4 Attach a Policy to a Role
+
+#5 PROGRAM TO GET LIST OF EXISTING IAM USERS
+
+response = iam.list_users()
+
+for user in response['Users']:
+    print(user['UserName'])
+
+#Calls list_users() to retrieve all IAM users.
+
+#5 PROGRAM TO GET LIST OF EXISTING IAM USERS
+
+#6 Delete an IAM User
+
+#response = iam.delete_user(UserName='skaluskar')
+
+#print(response)
+
+#Uses delete_user() to remove the IAM user.
+
+#6 Delete an IAM User
+
+#7 Create an IAM Group
+
+response = iam.create_group(GroupName='Security_Engineers')
+
+print(response)
+
+#Calls create_group() to create an IAM group named Software Engineers.
+
+#7 Create an IAM Group
+
+#8 Create an IAM Group
